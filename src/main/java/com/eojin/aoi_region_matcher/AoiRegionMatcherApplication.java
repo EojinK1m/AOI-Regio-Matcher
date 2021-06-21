bf6d59b41211c45ed9fb17ce4ac39358a06af193
eojink1m <eojin.dev@gmail,com>
2021-06-21 19:17:15 +0900

    INIT init project

---
+++ src/main/java/com/eojin/aoi_region_matcher/AoiRegionMatcherApplication.java
@@ -0,0 +1,13 @@
+package com.eojin.aoi_region_matcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoiRegionMatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AoiRegionMatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eojin/aoi_region_matcher/AoiRegionMatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eojin.aoi_region_matcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AoiRegionMatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
