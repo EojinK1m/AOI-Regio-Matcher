b207d9d3d39714e6f7f9fd917b02899282d396e8
eojink1m <eojin.dev@gmail,com>
2021-06-22 16:59:54 +0900

    REFACTOR change methods name to CamelCase

--- src/main/java/com/eojin/aoi_region_matcher/service/RegionService.java
+++ src/main/java/com/eojin/aoi_region_matcher/service/RegionService.java
@@ -4,8 +4,8 @@ package com.eojin.aoi_region_matcher.service;
 import com.eojin.aoi_region_matcher.model.Region;
 import com.eojin.aoi_region_matcher.payload.request.Coordinate;
 import com.eojin.aoi_region_matcher.payload.request.PostRegionRequest;
-import com.eojin.aoi_region_matcher.payload.request.PostRegionResponse;
-import com.eojin.aoi_region_matcher.repository.region.RegionRepository;
+import com.eojin.aoi_region_matcher.payload.response.PostRegionResponse;
+import com.eojin.aoi_region_matcher.repository.RegionRepository;
 import lombok.RequiredArgsConstructor;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Polygon;
@@ -20,20 +20,20 @@ import java.util.List;
 public class RegionService {
     private final RegionRepository regionRepository;
 
-    public PostRegionResponse create_region(PostRegionRequest request){
-        Polygon polygon = convert_area_into_polygon(request.getArea());
+    public PostRegionResponse createRegion(PostRegionRequest request){
+        Polygon polygon = convertCoordinatesToPolygon(request.getArea());
 
         Region region = Region
                 .builder()
                 .name(request.getName())
                 .area(polygon)
                 .build();
-        regionRepository.save(region);
+        region = regionRepository.save(region);
 
         return new PostRegionResponse(region.getId());
     }
 
-    private Polygon convert_area_into_polygon(List<Coordinate> area){
+    private Polygon convertCoordinatesToPolygon(List<Coordinate> area){
         GeometryFactory fact = new GeometryFactory(new PrecisionModel(), 4326);
         org.locationtech.jts.geom.Coordinate[] coordinates =
                 new org.locationtech.jts.geom.Coordinate[area.size()];
