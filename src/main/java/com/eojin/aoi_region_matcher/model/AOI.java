b8d35ab0762a5e190d6b336ee90b6fc7f8d00e8a
eojink1m <eojin.dev@gmail,com>
2021-06-22 13:53:13 +0900

    UPDATE create model, repository for AOI

---
+++ src/main/java/com/eojin/aoi_region_matcher/model/AOI.java
@@ -0,0 +1,28 @@
+package com.eojin.aoi_region_matcher.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.locationtech.jts.geom.Polygon;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class AOI {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Polygon area;
+
+}
---
+++ src/main/java/com/eojin/aoi_region_matcher/repository/AoiRepository.java
@@ -0,0 +1,15 @@
+package com.eojin.aoi_region_matcher.repository;
+
+import com.eojin.aoi_region_matcher.model.AOI;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AoiRepository extends JpaRepository<AOI, Integer> {
+
+    @Query(value = "select aa.id, aa.name, aa.area from aoi aa where ST_Overlaps(aa.area, (select area from region where id=:id))", nativeQuery = true)
+    List<AOI> findOverlappedAoiByRegionId(Integer id);
+}
