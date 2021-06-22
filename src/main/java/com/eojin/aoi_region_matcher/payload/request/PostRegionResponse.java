fafbc69483229098e65013c91bff17571836cce0
eojink1m <eojin.dev@gmail,com>
2021-06-22 10:06:11 +0900

    UPDATE create dto for region response

---
+++ src/main/java/com/eojin/aoi_region_matcher/payload/request/PostRegionResponse.java
@@ -0,0 +1,12 @@
+package com.eojin.aoi_region_matcher.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@Getter
+@AllArgsConstructor
+public class PostRegionResponse {
+    private Integer id;
+}
+
