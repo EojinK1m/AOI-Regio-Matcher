ce851f0d646bfbc4904254abf26922fea86c52c2
eojink1m <eojin.dev@gmail,com>
2021-06-23 15:31:47 +0900

    UPDATE postRegion set response status

--- src/main/java/com/eojin/aoi_region_matcher/controller/RegionController.java
+++ src/main/java/com/eojin/aoi_region_matcher/controller/RegionController.java
@@ -1,26 +1,24 @@
 package com.eojin.aoi_region_matcher.controller;
 
-import com.eojin.aoi_region_matcher.payload.request.PostRegionRequest;
-import com.eojin.aoi_region_matcher.payload.request.PostRegionResponse;
+import com.eojin.aoi_region_matcher.dto.request.PostRegionRequest;
+import com.eojin.aoi_region_matcher.dto.response.PostRegionResponse;
 import com.eojin.aoi_region_matcher.service.RegionService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-
 @RestController
-@RequestMapping("/region")
+@RequestMapping("/regions")
 @RequiredArgsConstructor
 public class RegionController {
     private final RegionService regionService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public PostRegionResponse postRegion(@RequestBody @Valid PostRegionRequest request){
-        return regionService.create_region(request);
+        return regionService.createRegion(request);
     }
 
 }
