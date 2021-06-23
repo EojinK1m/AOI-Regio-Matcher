e82fb338d8ac813b82b8b268471e7f19605f1229
eojink1m <eojin.dev@gmail,com>
2021-06-23 20:48:39 +0900

    REFACTOR fix PostAoi method name

--- src/main/java/com/eojin/aoi_region_matcher/controller/AoiController.java
+++ src/main/java/com/eojin/aoi_region_matcher/controller/AoiController.java
@@ -1,6 +1,7 @@
 package com.eojin.aoi_region_matcher.controller;
 
 import com.eojin.aoi_region_matcher.dto.request.PostAoiRequest;
+import com.eojin.aoi_region_matcher.dto.response.AoiResponse;
 import com.eojin.aoi_region_matcher.dto.response.PostAoiResponse;
 import com.eojin.aoi_region_matcher.service.AoiService;
 import lombok.RequiredArgsConstructor;
@@ -18,9 +19,8 @@ public class AoiController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public PostAoiResponse postRegion(@RequestBody @Valid PostAoiRequest request){
+    public PostAoiResponse postAoi(@RequestBody @Valid PostAoiRequest request){
         System.out.print(request.getName().getClass());
         return aoiService.createAoi(request);
     }
-
 }
