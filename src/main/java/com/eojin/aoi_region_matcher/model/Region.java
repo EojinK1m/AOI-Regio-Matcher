90fd5f98c64f058aecf64785573706e21419f8fc
eojink1m <eojin.dev@gmail,com>
2021-06-23 14:49:41 +0900

    UPDATE add unique attribute to name on AOI, Region

--- src/main/java/com/eojin/aoi_region_matcher/model/Region.java
+++ src/main/java/com/eojin/aoi_region_matcher/model/Region.java
@@ -20,7 +20,7 @@ public class Region {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
     @Column(nullable = false)
