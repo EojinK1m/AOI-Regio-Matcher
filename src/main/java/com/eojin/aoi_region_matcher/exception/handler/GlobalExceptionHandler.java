1fb64e8c617015ae0f381368cf65b43fd6211035
eojink1m <eojin.dev@gmail,com>
2021-06-23 16:37:02 +0900

    UPDATE GlobalExceptionHandler to handle validation exception

--- src/main/java/com/eojin/aoi_region_matcher/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/eojin/aoi_region_matcher/exception/handler/GlobalExceptionHandler.java
@@ -2,12 +2,36 @@ package com.eojin.aoi_region_matcher.exception.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> processValidationError(MethodArgumentNotValidException exception) {
+        BindingResult bindingResult = exception.getBindingResult();
+
+        StringBuilder builder = new StringBuilder();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            builder.append("[");
+            builder.append(fieldError.getField());
+            builder.append("](은)는 ");
+            builder.append(fieldError.getDefaultMessage());
+            builder.append(" 입력된 값: [");
+            builder.append(fieldError.getRejectedValue());
+            builder.append("]");
+        }
+
+        return new ResponseEntity<>(new ErrorResponse(
+                builder.toString()),
+                HttpStatus.valueOf(400)
+        );
+    }
+
     @ExceptionHandler()
     public ResponseEntity<ErrorResponse> handleBusinessException(final BusinessException e){
         final ErrorCode errorCode = e.getErrorCode();
@@ -17,4 +41,5 @@ public class GlobalExceptionHandler {
                 HttpStatus.valueOf(errorCode.getStatus())
         );
     }
+
 }
