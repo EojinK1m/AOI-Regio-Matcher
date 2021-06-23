dd0a01122f27d644be1b5acf634538392bf36c05
eojink1m <eojin.dev@gmail,com>
2021-06-23 21:31:51 +0900

    UPDATE add getNearestAoiByPoint()

--- src/main/java/com/eojin/aoi_region_matcher/repository/AoiRepository.java
+++ src/main/java/com/eojin/aoi_region_matcher/repository/AoiRepository.java
@@ -13,5 +13,13 @@ public interface AoiRepository extends JpaRepository<AOI, Integer> {
     @Query(value = "select aa.id, aa.name, aa.area from aoi aa where ST_Overlaps(aa.area, (select area from region where id=:id))", nativeQuery = true)
     List<AOI> findOverlappedAoiByRegionId(Integer id);
 
+    @Query(value =
+            "SELECT * " +
+            "FROM aoi " +
+            "ORDER BY ST_Distance(aoi.area, ST_SetSRID(ST_Point(:x, :y), 4326)) LIMIT 1;",
+            nativeQuery = true
+    )
+    AOI getNearestAoiByPoint(Double x, Double y);
+
     AOI getByName(String name);
 }
