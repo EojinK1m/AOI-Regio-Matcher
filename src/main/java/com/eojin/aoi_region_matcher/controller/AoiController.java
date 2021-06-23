376919e7ae91057182d280ef3dae3fab485698e0
eojink1m <eojin.dev@gmail,com>
2021-06-23 22:10:13 +0900

    UPDATE parsing queryPrams of getNearestAoi to handle exception

--- src/main/java/com/eojin/aoi_region_matcher/controller/AoiController.java
+++ src/main/java/com/eojin/aoi_region_matcher/controller/AoiController.java
@@ -3,12 +3,14 @@ package com.eojin.aoi_region_matcher.controller;
 import com.eojin.aoi_region_matcher.dto.request.PostAoiRequest;
 import com.eojin.aoi_region_matcher.dto.response.AoiResponse;
 import com.eojin.aoi_region_matcher.dto.response.PostAoiResponse;
+import com.eojin.aoi_region_matcher.exception.BadRequestException;
 import com.eojin.aoi_region_matcher.service.AoiService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/aois")
@@ -25,9 +27,18 @@ public class AoiController {
 
     @GetMapping
     public AoiResponse getNearestAoi(
-            @RequestParam(name = "lat") Double x,
-            @RequestParam(name = "long") Double y)
+            @RequestParam(name = "lat") Optional<String> xParameter,
+            @RequestParam(name = "long") Optional<String> yParameter)
     {
+        Double x, y;
+
+        try{
+            x = Double.valueOf(xParameter.get());
+            y = Double.valueOf(yParameter.get());
+        }catch (Exception e){
+            throw new BadRequestException();
+        }
+
         return aoiService.getNearestAoi(x, y);
     }
 
