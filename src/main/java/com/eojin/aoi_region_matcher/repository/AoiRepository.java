bbe85fb21d27eeee42678838e988491b2c85b40c
eojink1m <eojin.dev@gmail,com>
2021-06-23 14:54:51 +0900

    UPDATE add getByName to AoiRepository

--- src/main/java/com/eojin/aoi_region_matcher/repository/AoiRepository.java
+++ src/main/java/com/eojin/aoi_region_matcher/repository/AoiRepository.java
@@ -12,4 +12,6 @@ public interface AoiRepository extends JpaRepository<AOI, Integer> {
 
     @Query(value = "select aa.id, aa.name, aa.area from aoi aa where ST_Overlaps(aa.area, (select area from region where id=:id))", nativeQuery = true)
     List<AOI> findOverlappedAoiByRegionId(Integer id);
+
+    AOI getByName(String name);
 }
